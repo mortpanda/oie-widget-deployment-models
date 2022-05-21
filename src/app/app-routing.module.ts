3da6c6285fc1f3f81adbae322ed96efd929a3a19
Kent Nagao <31921593+mortpanda@users.noreply.github.com>
2022-05-21 11:27:27 +0900

    main
    main

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { StartComponent } from './start/start.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: LandingComponent },
+  { path: 'start', component: StartComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/start/start.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { OktaGetTokenService } from '../shared/okta/okta-get-token.service';
+import { ViewEncapsulation } from '@angular/core';
+import { OktaSDKAuthService } from '../shared/okta/okta-auth.service';
+import { OktaAuth } from '@okta/okta-auth-js'
+import { OktaConfigService } from "../shared/okta/okta-config.service";
+import { OktaWidgetService } from '../shared/okta/okta-widget.service';
+
+@Component({
+  selector: 'app-start',
+  templateUrl: './start.component.html',
+  styleUrls: ['./start.component.scss']
+})
+export class StartComponent implements OnInit {
+  public authService = new OktaAuth(this.OktaSDKAuthService.config);
+  strUserSession;
+  strThisUser;
+
+  constructor(
+    public OktaGetTokenService: OktaGetTokenService,
+    public OktaSDKAuthService: OktaSDKAuthService,
+    public OktaConfigService: OktaConfigService,
+    public OktaWidgetService: OktaWidgetService,
+  ) { }
+
+  async ngOnInit() {
+    
+    this.strUserSession = await this.authService.isAuthenticated();
+    console.log(this.strUserSession)
+    switch (this.strUserSession == true) {
+      case false:
+        window.location.replace(this.OktaConfigService.strPostLogoutURL);
+      case true:
+        this.strThisUser = await this.authService.token.getUserInfo()
+          .then(function (user) {
+            return user
+          })
+          .catch((err) => {
+            console.log(err);
+            window.location.replace(this.OktaConfigService.strPostLogoutURL);
+           })
+        break;
+    }
+    console.log(this.strThisUser)
+
+  }
+
+}
